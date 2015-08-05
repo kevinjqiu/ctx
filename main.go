e1e27a86fb64a32e240bbfeb469239da3543994c
Kevin Qiu <kevin@idempotent.ca>
2015-08-05 00:12:53 -0400

    Simple timer

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
+	"io/ioutil"
 	"os"
+	"strconv"
+	"time"
 )
 
 func newContext(c *cli.Context) {
+	localFileName := os.ExpandEnv("${HOME}/.ctx")
+	now := time.Now()
+	ioutil.WriteFile(localFileName, []byte(strconv.FormatInt(now.UnixNano(), 10)), 0644)
 }
 
 func info(c *cli.Context) {
+	localFileName := os.ExpandEnv("${HOME}/.ctx")
+	now := time.Now()
+	content, e1 := ioutil.ReadFile(localFileName)
+	if e1 != nil {
+		fmt.Printf("Cannot read %s", localFileName)
+		return
+	}
+
+	then, e2 := strconv.ParseUint(string(content), 10, 64)
+	if e2 != nil {
+		fmt.Printf("Cannot convert %s to a timestamp", content)
+		return
+	}
+
+	duration := time.Duration(uint64(now.UnixNano()) - then)
+
+	fmt.Println(duration)
 }
 
 func main() {
