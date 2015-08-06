1f41921d7e540bc0dae385cf3f798f09023d78bb
Kevin Qiu <kevin@idempotent.ca>
2015-08-05 23:49:15 -0400

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-fn main() {
-    let mut range = 0..10;
+extern crate clap;
 
-    loop {
-        match range.next() {
-            Some(x) => {
-                println!("{}", x);
-            },
-            None => { break }
-        }
+use clap::{App, SubCommand};
+
+fn main() {
+    let m = App::new("ctx")
+        .about("A CLI utility to manage context")
+        .author("Kevin Qiu <kevin@idempotent.ca>")
+        .subcommand_required(true)
+        .subcommand(SubCommand::with_name("switch"))
+            .about("Switch context")
+        .get_matches();
+
+    match m.subcommand() {
     }
 }
